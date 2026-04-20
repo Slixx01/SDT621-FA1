602974874ab6acb7124ca16b2491c1becdb27b7c
Slixx01 <chrissiejohnson73@gmail.com>
2026-04-20 22:18:25 +0200

    Completed Student Results Question 1

---
+++ SectionA_Q1_StudentResults/StudentResults.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class StudentResults
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Student name");
+        string Name = Console.ReadLine()!;
+        if (Name == null)
+        {
+            Console.WriteLine("Please enter a name");
+        }
+
+
+        List<int> StudentMarks = new List<int>();
+
+
+        for (int i = 1; i < 4; i++)
+        {
+            Console.WriteLine($"Enter mark for Subject {i} :");
+            StudentMarks.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int SumMarks = StudentMarks.Sum();
+        double AverageMarks = Math.Round(StudentMarks.Average(), 2);
+        string StudentResult = (AverageMarks >= 50) ? "Pass" : "Fail";
+
+
+        Console.WriteLine(
+            $"""
+            ==== STUDENT RESULTS ====
+            Student Name: {Name}
+            Total Marks: {SumMarks} 
+            Average Makrs: {AverageMarks}
+            Result: {StudentResult}
+            Results Issued At: {DateTime.Now}
+            """);
+
+        Console.WriteLine("Press Any key to exit....");
+        Console.ReadKey();
+
+
+    }
+
+}
+
+
+
+
+
